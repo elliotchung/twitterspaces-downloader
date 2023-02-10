c2d95006613f15c41f5a288b9d1ca728fa298851
Elliot <101564632+elliotchung@users.noreply.github.com>
2023-02-10 14:49:12 +0800

    Update outputconcat.py

--- outputconcat.py
+++ outputconcat.py
@@ -1,4 +1,4 @@
 video_files = [f"output{i}.ts" for i in range(1, 5)]
 input_string = "|".join(video_files)
-final_command = f'ffmpeg -i "concat:{input_string}" -c copy 10feb.aac'
+final_command = f'ffmpeg -i "concat:{input_string}" -c copy Final.aac'
 print(final_command)
